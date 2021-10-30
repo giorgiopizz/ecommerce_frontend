0a291025dbb1d01cff1efb5e1c5c3346fda636e3
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2021-10-30 19:03:43 +0200

    asdad

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
+import ProductComp from "../components/ProductComp";
 import products from "../services/prodotti";
-
-type Prodotto = {
-	id: string;
-	titolo: string;
-	prezzo: string;
-};
+import { Product } from "../types/Product";
+// type Prodotto = {
+// 	id: string;
+// 	titolo: string;
+// 	prezzo: string;
+// };
 function Home() {
-	const [prodotti, setProdotti] = useState<Prodotto[]>([]);
+	const [prodotti, setProdotti] = useState<Product[]>([]);
 	useEffect(() => {
 		products.getAll().then((res) => {
-			setProdotti(res.content);
+			setProdotti(res.data);
 		});
 	}, []);
-	return <div></div>;
+	return (
+		<div>
+			{prodotti.map((prodotto, index) => (
+				<ProductComp key={index} prodotto={prodotto} />
+			))}
+		</div>
+	);
 }
 
 export default Home;
