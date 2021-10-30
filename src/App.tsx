a7411da646afcd0f09ddcf7d7baf9b813914751b
giorgio <giorgio.pizzati@gmail.com>
2021-10-30 11:10:22 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@
 
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import Home from "./screens/Home";
+import Orders from "./screens/Orders";
 export default function App() {
 	return (
 		<Router>
@@ -32,11 +33,11 @@ export default function App() {
 				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 				<Switch>
-					<Route path='/about'>
-						<About />
-					</Route>
+					{/* <Route path='/about'>
+						< />
+					</Route> */}
 					<Route path='/users'>
-						<Users />
+						<Orders />
 					</Route>
 					<Route path='/'>
 						<Home />
